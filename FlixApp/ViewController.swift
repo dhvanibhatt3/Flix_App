7f6f5054d29c0811246286881c50892428347d82
dhvanibhatt3 <72041537+dhvanibhatt3@users.noreply.github.com>
2022-03-04 21:28:19 -0800

    Added new features in existing FlixApp for Assignment 2

--- FlixApp/ViewController.swift
+++ FlixApp/ViewController.swift
@@ -36,7 +36,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
                  self.movies = dataDictionary ["results"] as! [[String:Any]]
                  self.tableView.reloadData()
-                 print(dataDictionary)
+                // print(dataDictionary)
                  
                     // TODO: Get the array of movies
                     // TODO: Store the movies in a property to use elsewhere
@@ -69,6 +69,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
                     cell.posterView.af.setImage(withURL: posterUrl!)
             return cell
         }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("Loading up the Detais Screen")
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
 
 
 }
